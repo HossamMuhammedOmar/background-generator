b9bf18a27b3305a7db5cabbe6fd80a83bcaea1ec
Your Namecls <you@example.com>
2020-07-15 20:16:00 +0200

    adding starting project

---
+++ script.js
@@ -0,0 +1,15 @@
+let colorQuery = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradint");
+
+function addGradientColor() {
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+
+  colorQuery.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", addGradientColor);
+
+color2.addEventListener("input", addGradientColor);
